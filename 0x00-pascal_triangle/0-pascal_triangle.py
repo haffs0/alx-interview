9795395379b6d663100efdb72be26155e267e182
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-03-22 15:26:05 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,13 @@
 create a pascal triangle function
 """
 
+
 def factorial(n):
     fact = 1
     for i in range(1, n+1):
         fact = fact * i
     return (fact)
+
 
 def pascal_triangle(n):
     """
