248e75d84f646192f1a30ed6d68f9bd7ce5f6e3d
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-04-20 11:30:38 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,25 +5,20 @@
 def validUTF8(data):
     """return True if its a validUTF8 or False otherwise"""
     data_len = len(data)
+    count = 0
     for i in range(data_len):
-        curr = data[i]
-        byte_type = get_type(curr)
-        if byte_type == 0:
-            continue
-        elif byte_type > 1 and i + byte_type <= data_len:
-            while byte_type > 1:
-                if get_type(data[++i]) != 1:
-                    return False
-                byte_type -= 1
+        x = data[i]
+        if not count:
+            if (x >> 5) == 0b110:
+                count = 1
+            elif (x >> 4) == 0b1110:
+                count = 2
+            elif (x >> 3) == 0b11110:
+                count = 3
+            elif (x >> 7) != 0:
+                return False
         else:
-            return False
-    return True
-
-
-def get_type(num: int):
-    """return type"""
-    masks = [128, 64, 32, 16, 8]
-    for i in range(5):
-        if (masks[i] & num) == 0:
-            return i
-    return -1
+            if (x >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
